fc7a6c22aada3ce2f4d91a2429e1426679f48ef8
Alex Seitsinger <16756928+alexseitsinger@users.noreply.github.com>
2019-09-08 14:35:14 -0400

    docs: Updates JSDoc.

--- src/simplePopover/index.js
+++ src/simplePopover/index.js
@@ -4,6 +4,51 @@ import _ from "underscore"
 
 import { Container, Popover, PopoverMessage } from "./elements"
 
+/**
+ * A simple popover element.
+ *
+ * @returns {function} A component.
+ *
+ * @param {object} props
+ * @param {boolean} props.isPopoverVisible
+ * Determines if the popover is visible or not.
+ * @param {function} props.setPopoverVisible
+ * The function to invoke to change the isPopoverVisible value.
+ * @param {string} props.popoverMessage
+ * The text string to display in the popover.
+ * @param {object} [props.popoverStyle={}]
+ * Extra css styles to apply to the popover.
+ * @param {string} [props.popoverOrigin=right]
+ * The direction that the popover message is displayed from.
+ * @param {function} props.children
+ * The content to display which triggers a popover.
+ *
+ * @example
+ * import React from "react"
+ * import { connect } from "react-redux"
+ * import { SimplePopover } from "@alexseitsinger/react-simple-popover"
+ *
+ * const App = ({ isPopoverVisible, setPopoverVisible, ...props }) => {
+ *  return (
+ *    <SimplePopover
+ *      isPopoverVisible={isPopoverVisible}
+ *      setPopoverVisible={setPopoverVisible}
+ *      popoverMessage={"This is the text that gets displayed"}>
+ *      <div>This is the content that has a popover.</div>
+ *    </SimplePopover>
+ *  )
+ * }
+ *
+ * const mapDispatch = dispatch => ({
+ *   setPopoverVisible: bool => dispatch(setPopoverVisible(bool)),
+ * })
+ *
+ * const mapState = state => ({
+ *   isPopoverVisible: state.app.isPopoverVisible,
+ * })
+ *
+ * export default connect(mapState, mapDispatch)(App)
+ */
 export class SimplePopover extends React.Component {
   static propTypes = {
     isPopoverVisible: PropTypes.bool.isRequired,
